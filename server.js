bd1c1988a64bc2277b16422383bc798696b6cedb
kaleem316 <mohammad.kaleemmak@gmail.com>
2026-05-03 14:33:01 +0400

    brand name v1 added

--- server.js
+++ server.js
@@ -359,7 +359,7 @@ app.post("/stitch", async (req, res) => {
 
 		  for (let i = startIndex; i < totalClips; i++) {
 			// skip last if it's outro
-			if (req.body.outroImage && i === totalClips - 1) break;
+			if (req.body.outroImage && i === totalClips - 1) continue;
 
 			const inputClip = normFiles[i];
 			const outputClip = path.join(tmp, `brand_${i}.mp4`);
