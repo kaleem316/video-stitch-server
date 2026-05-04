6f49f35bdf64e8742891b0de268d5f2b6a8eebc0
kaleem316 <mohammad.kaleemmak@gmail.com>
2026-05-04 16:14:50 +0400

    brand name v13 added

--- server.js
+++ server.js
@@ -244,8 +244,8 @@ async function addBrandText(input, output, brand, width, height, brandTextStyle)
   const phone = (brand.phone || "").replace(/'/g, "\\'").replace(/:/g, "\\:");
   const location = (brand.location || "").replace(/'/g, "\\'").replace(/:/g, "\\:");
 
-  const nameSize = Math.round(height * 0.02);
-  const bottomSize = Math.round(height * 0.028);
+  const nameSize = Math.round(height * 0.04);
+  const bottomSize = Math.round(height * 0.01);
 
   let nameColor = "white";
   let boxColor = "black@0.5";
